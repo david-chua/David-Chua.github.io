c9ae340445e0f6700c164c9776647665526a1dfb
dp_chua <dp_chua@hotmail.com>
2017-03-21 00:28:54 -0400

    added Jquery

---
+++ script.js
@@ -0,0 +1,12 @@
+$(function(){
+   setTimeout(function() {
+     $('.fly-text').removeClass('hidden');
+   }, 700);
+})();
+
+
+// $(function(){
+//    showbutton(function() {
+//      $('.Button').removeClass('hidden');
+//    }, 700);
+// })();
